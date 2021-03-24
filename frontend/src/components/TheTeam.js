b7ba6f5b9251e7a796beedda6282b8ca3aea0dc6
tmonis <tmonis26@gmail.com>
2021-03-24 13:23:26 -0500

    Added labels for the card.

--- frontend/src/components/TheTeam.js
+++ frontend/src/components/TheTeam.js
@@ -9,36 +9,54 @@ const TheTeam = (props) => {
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <CardTitle tag="h5">Team Member #1</CardTitle>
+          <CardText>Major:</CardText>
+          <CardText>School Name:</CardText>
+          <CardText>Year:</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+          <CardTitle tag="h5">Team Member #2</CardTitle>
+          <CardText>Major:</CardText>
+          <CardText>School Name:</CardText>
+          <CardText>Year:</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <CardTitle tag="h5">Team Member #3</CardTitle>
+          <CardText>Major:</CardText>
+          <CardText>School Name:</CardText>
+          <CardText>Year:</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+          <CardTitle tag="h5">Team Member #4</CardTitle>
+          <CardText>Major:</CardText>
+          <CardText>School Name:</CardText>
+          <CardText>Year:</CardText>
+          <Button>Button</Button>
+        </CardBody>
+        <CardBody>
+          <CardTitle tag="h5">Team Member #5</CardTitle>
+          <CardText>Major:</CardText>
+          <CardText>School Name:</CardText>
+          <CardText>Year:</CardText>
+          <Button>Button</Button>
+        </CardBody>
+        <CardBody>
+          <CardTitle tag="h5">Team Member #6</CardTitle>
+          <CardText>Major:</CardText>
+          <CardText>School Name:</CardText>
+          <CardText>Year:</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
