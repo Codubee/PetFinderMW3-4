706e672621ca70ff991cfbcf06855d24dc77a8b9
tmonis <tmonis26@gmail.com>
2021-03-24 12:56:49 -0500

    This is the Teams Page.

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import TheTeam from '../components/TheTeam';
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
+            <TheTeam/>
             <h1>Landing Page Component</h1>
         </div>
     )
