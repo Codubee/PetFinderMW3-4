65e4ccbb1bdd43b15e4270fb79d7916940ba7764
Bo Mendez <mendez.bo@northeastern.edu>
2021-03-25 13:17:42 -0700

    adding NavBar

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import NavigationBar from '../components/NavBar';
 
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
-            <h1>Landing Page Component</h1>
+            <NavigationBar></NavigationBar>
         </div>
     )
 }
