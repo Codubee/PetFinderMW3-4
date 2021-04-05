58c663f3f081222e4a1fedfc37686ffb4101d094
tmonis <tmonis26@gmail.com>
2021-04-05 14:22:17 -0500

    Updated changes from PR, added github and linkedin links.

--- frontend/src/components/TheTeam.js
+++ frontend/src/components/TheTeam.js
@@ -1,66 +1,60 @@
 import React from 'react';
 import '../styles/TeamStyle.css';
-import img1 from '../assets/images/Bo_Mendez.jpg';
-import img2 from '../assets/images/Chelsea_Heredia.jpg';
-import img3 from '../assets/images/Tritian_Monis.png';
-import img4 from '../assets/images/Roshin_Lal.jpg';
-import img5 from '../assets/images/placement.png';
+import Chelsea_pic from '../assets/images/Chelsea_Heredia.jpg';
+import Tritian_pic from '../assets/images/Tritian_Monis.png';
+import Roshin_pic from '../assets/images/Roshin_Lal.jpg';
+import Anjelica_pic from '../assets/images/Anjelica_Avorque.jpg';
 import {
-  Card, CardImg, CardTitle, CardText, CardBody, Row
+  Card, CardImg, CardTitle, CardText, CardLink, CardBody, Row
 } from 'reactstrap';
-const TheTeam = (props) => {
+
+function TheTeam() {
+
+  function teamInfo(pic, first, last, major, school, year, linkedIn, github) {
+    this.pic = pic;
+    this.first = first;
+    this.last = last;
+    this.major = major;
+    this.school = school;
+    this.year = year;
+    this.linkedIn = linkedIn;
+    this.github = github;
+  }
+
+  var team = [
+    new teamInfo(Chelsea_pic, 'Chelsea', 'Heredia', 'Computer Science', 'University of Texas at Dallas', 'Freshman (2020-2024)', 'https://www.linkedin.com/in/chelsea-heredia/', 'https://github.com/chelseah02'),
+    new teamInfo(Tritian_pic, 'Tritian', 'Monis', 'Information Technology & Systems', 'University of Texas at Dallas', 'Junior (2018-2022)', 'https://www.linkedin.com/in/tritian-monis/', 'https://github.com/tmonis'),
+    new teamInfo(Roshin_pic, 'Roshin', 'Lal', 'Computer Engineering', 'Texas A&M University', 'Sophomore (2019-2023)', 'https://www.linkedin.com/in/roshin-lal/', 'https://github.com/Roshinl'),
+    new teamInfo(Anjelica_pic, 'Anjelica', 'Avorque', 'Computer Science', 'University of Texas at Dallas', 'Junior (2018-2022)', 'https://www.linkedin.com/in/anjelica-avorque/', 'https://github.com/evanjelica')
+  ]
+
   return (
+    <div className='center'>
+      <h1 className='team-welcome-title'>Meet The Team</h1>
       <Row className="justify-content-sm-center">
-      <Card>
-        <CardImg variant="top" src={img1} alt="Bo Mendez" />
-        <CardBody>
-          <CardTitle tag="h6">Bo Mendez</CardTitle>
-          <CardText tag="h6">Major: Computer Science</CardText>
-          <CardText tag="h6">School: Northeastern University</CardText>
-          <CardText tag="h6">Year: 1st Year Master's student</CardText>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg src={img2} alt="Chelsea Heredia" />
-        <CardBody>
-          <CardTitle tag="h6">Chelsea Heredia</CardTitle>
-          <CardText tag="h6">Major: Computer Science</CardText>
-          <CardText tag="h6">School: The University of Texas at Dallas</CardText>
-          <CardText tag="h6">Year: Freshman (2020-2024)</CardText>
-        </CardBody>
-      </Card>
+        {
+          team.map((teamMember, index) => (
+              <Card className='team-card' key={index}>
+                <CardImg className='team-card-pic' src={teamMember.pic} />
+                <CardBody className='team-card-body'>
+                  <CardTitle className='team-card-title' tag="h6">{teamMember.first + " " + teamMember.last}</CardTitle>
+                  <CardText className='team-card-text' tag="h6">{"Major: " + teamMember.major}</CardText>
+                  <CardText className='team-card-text' tag="h6">{"School: " + teamMember.school}</CardText>
+                  <CardText className='team-card-text' tag="h6">{"Year: " + teamMember.year}</CardText>
+                  <CardLink className='team-card-linkedin' href={teamMember.linkedIn}>LinkedIn</CardLink>
+                  <CardLink className='team-card-github' href={teamMember.github}>GitHub</CardLink>
 
-      <Card>
-        <CardImg src={img3} alt="Tritian Monis" />
-        <CardBody>
-          <CardTitle tag="h6">Tritian Monis</CardTitle>
-          <CardText tag="h6">Major: Information Technology & Systems</CardText>
-          <CardText tag="h6">School: The University of Texas at Dallas</CardText>
-          <CardText tag="h6">Year: Junior (2018-2022)</CardText>
-        </CardBody>
-      </Card>
 
-      <Card>
-        <CardImg src={img4} alt="Roshin Lal" />
-        <CardBody>
-          <CardTitle tag="h6">Roshin Lal</CardTitle>
-          <CardText tag="h6">Major: Computer Engineering</CardText>
-          <CardText tag="h6">School: Texas A&M University</CardText>
-          <CardText tag="h6">Year: Sophomore (2019-2023)</CardText>
-        </CardBody>
-      </Card>
-      
-      <Card>
-        <CardImg src={img5} alt="Pending" />
-        <CardBody>
-          <CardTitle tag="h6">Anjelica Avorque</CardTitle>
-          <CardText tag="h6">Major: Computer Science</CardText>
-          <CardText tag="h6">School: The University of Texas at Dallas</CardText>
-          <CardText tag="h6">Year: Junior (2018-2022)</CardText>
-        </CardBody>
-      </Card>
-    </Row>
+
+                </CardBody>
+              </Card>
+          ))
+        }
+
+
+      </Row>
+    </div>
   );
-};
+}
 
-export default TheTeam;
+  export default TheTeam;
