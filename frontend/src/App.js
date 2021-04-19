3c44addfa8c441a73da7d5a8906c610bc598c2ea
codubee <info@codubee.com>
2021-04-19 15:15:31 -0500

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import NavigationBar from './components/NavBar';
 import LandingPage from './views/LandingPage'
+import Match from './views/Match'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 function App() {
   return (
     <div>
-      <NavigationBar/>
-      <LandingPage/>
+      <NavigationBar />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/match">
+            <Match />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/views/Match.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Match(){
+    return(
+        <h1>Match page</h1>
+    )
+}
+
+export default Match
