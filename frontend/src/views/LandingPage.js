3d0fb0d019adb8ab4b7945f839af98b7bc0c2e62
Roshin Lal <66693974+Roshinl@users.noreply.github.com>
2021-04-19 15:17:18 -0500

    MW-4 Features (#8)
    * Features file with carousel and images
    
    * Renamed assets to images, moves styles to css, changed to a function component
    
    * Added landing page to app.js
    
    * Centered pictures and increased title font size on carousel
    
    Co-authored-by: Roshin Lal <roshinl1207@tamu.edu>
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Team from '../components/Team';
 import Explanation from '../components/Explanation';
+
 import Features from '../components/Features';
 import Tools from '../components/Tools';
 
