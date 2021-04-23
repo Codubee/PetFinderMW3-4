79faee0f22bcc98e48492cf92c0bd2555de60a26
Codubee <70549347+Codubee@users.noreply.github.com>
2021-04-22 23:08:20 -0500

    Added the yes or no buttons (#11)

--- frontend/src/views/Match.js
+++ frontend/src/views/Match.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Button} from 'reactstrap'
 
+class Match extends React.Component{
+    constructor(props){
+        super(props)
 
-function Match(){
-    return(
-        <h1>Match page</h1>
-    )
+    }
+    render(){
+        return(
+            <div>
+                {/* Image component beneath this comment*/}
+                <Button color="success">Yes</Button>
+                <Button color="danger">No</Button>
+                {/* Results components will go below this comment */}
+            </div>
+        )
+    }
+    
 }
 
 export default Match
