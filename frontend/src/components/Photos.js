e939a513323c615f48a25b5a5873a71deda8c3af
codubee <info@codubee.com>
2021-05-17 15:33:52 -0500

    Centerd the image

--- frontend/src/components/Photos.js
+++ frontend/src/components/Photos.js
@@ -2,28 +2,16 @@
 
 import React from 'react';
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle,Row,Col
+  Card, CardImg
 } from 'reactstrap';
 import '../style/Photos.css'
-function ViewPhotos(props){
-console.log(props)
-  return (
-    <div>
-      <Row>   
-        <Col sm={{size:3,offset:4}}>  
-        <Card className="text-center">
-        <CardImg src= {props.img_url} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Animal Name</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>Animal Description</CardText>
-        </CardBody>
-      </Card>
-      </Col>
-      </Row>
 
-    </div>
+function ViewPhotos(props) {
+  return (
+
+      <Card className="center-div">
+        <CardImg src={props.img_url} alt="Card image cap" />
+      </Card>
   );
 }
 
